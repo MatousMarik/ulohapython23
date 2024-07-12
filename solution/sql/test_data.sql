f6851ed63f550bdd155f49f634775304514a4249
MatousMarik <matymar@email.cz>
2024-07-12 10:51:34 +0200

    some test data

---
+++ solution/sql/test_data.sql
@@ -0,0 +1,4 @@
+INSERT INTO domain (domain_name, registered_at, unregistered_at) VALUES
+('example', '2023-01-01 00:00:00', NULL),
+('test', '2023-02-01 00:00:00', '2023-03-01 00:00:00'),
+('foo', '2023-03-01 00:00:00', NULL);
