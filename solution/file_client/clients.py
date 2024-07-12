c02f5202b3f015328f3ba61715d37f5e46ef9963
MatousMarik <matymar@email.cz>
2024-07-12 11:39:04 +0200

    grpc client

--- solution/file_client/clients.py
+++ solution/file_client/clients.py
@@ -1,3 +1,6 @@
+import grpc
+from file_client.service_file_pb2 import StatRequest, ReadRequest, Uuid
+from file_client.service_file_pb2_grpc import FileStub
 
 
 class Client:
@@ -9,3 +12,30 @@ class Client:
 
     def get_content(self, uuid):
         pass
+
+
+class GrpcClient(Client):
+    def __init__(self, address) -> None:
+        super().__init__(address)
+        self.channel = grpc.insecure_channel(address)
+        self.stub = FileStub(self.channel)
+
+    def get_metadata(self, uuid):
+        request = StatRequest(uuid=Uuid(value=uuid))
+        response = self.stub.stat(request)
+        return {
+            "create_datetime": response.data.create_datetime.ToJsonString(),
+            "size": response.data.size,
+            "mimetype": response.data.mimetype,
+            "name": response.data.name
+        }
+
+    def get_content(self, uuid):
+        request = ReadRequest(uuid=Uuid(value=uuid))
+        response = self.stub.read(request)
+        content = b""
+        for chunk in response:
+            content += chunk.data.data
+        return {
+            'content': content,
+        }
