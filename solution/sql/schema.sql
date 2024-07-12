77523de183b61965a750721a434c0d328ebdc35c
MatousMarik <matymar@email.cz>
2024-07-12 10:42:50 +0200

    domain_flag table

--- solution/sql/schema.sql
+++ solution/sql/schema.sql
@@ -45,3 +45,45 @@ CREATE TRIGGER check_unique_active_domain_update
     BEFORE UPDATE ON domain
     FOR EACH ROW
     EXECUTE FUNCTION check_unique_active_domain();
+
+-- Create the domain_flag table
+CREATE TABLE domain_flag (
+    id SERIAL PRIMARY KEY,
+    domain_id INT REFERENCES domain(id) ON DELETE CASCADE,
+    flag VARCHAR(50) NOT NULL CHECK (flag IN ('EXPIRED', 'OUTZONE', 'DELETE_CANDIDATE')),
+    start_time TIMESTAMP NOT NULL,
+    end_time TIMESTAMP
+);
+
+-- Indexe for performance optimization
+CREATE INDEX idx_flag_domain ON domain_flag(domain_id);
+
+/* Never change inserted records with one exception: if upper limit is unbounded, it can be set to specific 
+   point in time but never to the past (at the time the change is made).
+   => only null end_time rows can be modified */
+CREATE OR REPLACE FUNCTION forbid_flag_column_update_only_upper_limit()
+    RETURNS TRIGGER 
+    LANGUAGE PLPGSQL
+    AS
+$$
+BEGIN
+	IF (OLD.end_time IS NOT NULL OR     -- upper limit is not unbounded
+        (OLD.end_time IS NULL AND       -- upper limit unbounded
+            (NEW.end_time < now()       -- can't be set to past
+            OR NEW.id <> OLD.id         -- rest can't be modified
+            OR NEW.domain_id <> OLD.domain_id
+            OR NEW.flag <> OLD.flag
+            OR NEW.start_time <> OLD.start_time
+            )))
+    THEN
+		RAISE EXCEPTION 'Modification not allowed';
+	END IF;
+
+	RETURN NEW;
+END;
+$$;
+
+CREATE TRIGGER forbid_flag_modification
+    BEFORE UPDATE ON domain_flag
+    FOR EACH ROW
+    EXECUTE FUNCTION forbid_flag_column_update_only_upper_limit();
