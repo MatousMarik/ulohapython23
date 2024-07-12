f5d123a86af2e417d9f8265d5e727ea6554f075d
MatousMarik <matymar@email.cz>
2024-07-12 11:24:10 +0200

    file client class

---
+++ solution/file_client/clients.py
@@ -0,0 +1,11 @@
+
+
+class Client:
+    def __init__(self, address) -> None:
+        self.address = address
+
+    def get_metadata(self, uuid):
+        pass
+
+    def get_content(self, uuid):
+        pass
