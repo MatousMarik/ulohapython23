be9aa2f211ae2d4b5d37b8a91ea4acdbdaa200ff
MatousMarik <matymar@email.cz>
2024-07-12 13:19:54 +0200

    rest client

--- solution/file_client/clients.py
+++ solution/file_client/clients.py
@@ -2,6 +2,8 @@ import grpc
 from file_client.service_file_pb2 import StatRequest, ReadRequest, Uuid
 from file_client.service_file_pb2_grpc import FileStub
 
+import requests
+
 
 class Client:
     def __init__(self, address) -> None:
@@ -16,7 +18,6 @@ class Client:
 
 class GrpcClient(Client):
     def __init__(self, address) -> None:
-        super().__init__(address)
         self.channel = grpc.insecure_channel(address)
         self.stub = FileStub(self.channel)
 
@@ -42,4 +43,19 @@ class GrpcClient(Client):
 
 
 class RestClient(Client):
-    pass
+    def __init__(self, address) -> None:
+        super().__init__(address)
+
+    def get_metadata(self, uuid):
+        url = f"{self.address}file/{uuid}/stat/"
+        response = requests.get(url)
+        response.raise_for_status()
+        return response.json()
+
+    def get_content(self, uuid):
+        url = f"{self.address}file/{uuid}/read/"
+        response = requests.get(url)
+        response.raise_for_status()
+        return {
+            'content': response.content,
+        }
