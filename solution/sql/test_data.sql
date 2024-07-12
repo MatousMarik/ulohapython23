de5c5624098091a2555788a99dbfa1a2cde14ef5
MatousMarik <matymar@email.cz>
2024-07-12 11:05:30 +0200

    more test data

--- solution/sql/test_data.sql
+++ solution/sql/test_data.sql
@@ -1,8 +1,9 @@
 INSERT INTO domain (domain_name, registered_at, unregistered_at) VALUES
 ('example', '2023-01-01 00:00:00', NULL),
 ('test', '2023-02-01 00:00:00', '2023-03-01 00:00:00'),
-('foo', '2023-03-01 00:00:00', NULL)
-('example', '2022-02-01 00:00:00', '2022-03-01 00:00:00');
+('foo', '2023-03-01 00:00:00', NULL),
+('example', '2022-02-01 00:00:00', '2022-03-01 00:00:00'),
+('bar1.134.f', '2023-03-01 00:00:00', NULL);
 
 /* invalid data
 INSERT INTO domain (domain_name, registered_at, unregistered_at) VALUES
@@ -10,6 +11,15 @@ INSERT INTO domain (domain_name, registered_at, unregistered_at) VALUES
 
 INSERT INTO domain (domain_name, registered_at, unregistered_at) VALUES
 ('test', '2022-02-01 00:00:00', NULL);
+
+INSERT INTO domain (domain_name, registered_at, unregistered_at) VALUES
+('.p', '2022-02-01 00:00:00', NULL);
+
+INSERT INTO domain (domain_name, registered_at, unregistered_at) VALUES
+('.nono', '2022-02-01 00:00:00', NULL);
+
+INSERT INTO domain (domain_name, registered_at, unregistered_at) VALUES
+('test.', '2022-02-01 00:00:00', NULL);
 
 UPDATE domain
 SET unregistered_at = NULL
