604dc0a99763b7812aacbe605f265210719325d1
MatousMarik <matymar@email.cz>
2024-07-12 11:40:37 +0200

    create main with help

---
+++ solution/file_client/main.py
@@ -0,0 +1,23 @@
+import argparse
+from file_client.clients import GrpcClient
+
+
+def main():
+    parser = argparse.ArgumentParser(description='File Client')
+    parser.add_argument('command', choices=[
+                        'stat', 'read'], help='Request to run')
+    parser.add_argument('uuid', help='UUID of the file')
+    parser.add_argument('--backend', choices=['grpc', 'rest'], default='grpc',
+                        help='Set a backend to be used, choices are grpc and rest. Default is grpc.')
+    parser.add_argument('--grpc-server', default='localhost:50051',
+                        help='Set a host and port of the gRPC server. Default is localhost:50051.')
+    parser.add_argument('--base-url', default='http://localhost/',
+                        help='Set a base URL for a REST server. Default is http://localhost/.')
+    parser.add_argument('--output', default='-',
+                        help='Set the file where to store the output. Default is -, i.e. the stdout.')
+
+    args = parser.parse_args()
+
+
+if __name__ == '__main__':
+    main()
