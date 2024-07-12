0715ae10b35fd2dfa31c6e8c2bbb3c5e6ba2202e
MatousMarik <matymar@email.cz>
2024-07-12 11:09:40 +0200

    second query

--- solution/sql/queries.sql
+++ solution/sql/queries.sql
@@ -14,3 +14,12 @@ WHERE d.registered_at < now()
             AND f.start_time < now()
             AND COALESCE(f.end_time, 'infinity'::timestamp) > now()
     );
+
+-- Domains, that had EXPIRED and OUTZONE flags. For simplicity I assume host www and TLD cz
+SELECT DISTINCT 'www.' || d.domain_name || '.cz' as fully_qualified_domain_name
+FROM domain d
+JOIN domain_flag fe ON d.id = fe.domain_id AND fe.flag = 'EXPIRED'
+JOIN domain_flag fo ON d.id = fo.domain_id AND fo.flag = 'OUTZONE'
+WHERE COALESCE(fe.end_time, 'infinity'::timestamp) < now()
+AND COALESCE(fo.end_time, 'infinity'::timestamp) < now()
+GROUP BY d.domain_name;
--- solution/sql/test_data.sql
+++ solution/sql/test_data.sql
@@ -25,3 +25,9 @@ UPDATE domain
 SET unregistered_at = NULL
 WHERE domain_name = 'example';
 */
+
+INSERT INTO domain_flag (domain_id, flag, start_time, end_time) VALUES
+(1, 'EXPIRED', '2023-01-01 00:00:00', '2023-01-10 00:00:00'),
+(2, 'OUTZONE', '2023-02-01 00:00:00', '2023-02-15 00:00:00'),
+(2, 'EXPIRED', '2023-02-10 00:00:00', '2023-02-20 00:00:00'),
+(3, 'OUTZONE', '2023-03-01 00:00:00', '2023-03-15 00:00:00');
